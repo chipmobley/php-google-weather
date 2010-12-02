706ba4c38cc4d50c5217b3840c4a3e691e2a0912
chipmobley <chip@riomedinasoap.com>
2010-12-01 22:04:45 -0600

    Add current wind condition function

--- weather.php
+++ weather.php
@@ -4,6 +4,7 @@ class GoogleWeather {
 	private $google_xml;
 	private $location;
 	private $current_temp;
+	private $current_wind;
 	
 	function __construct($zip_code){
 		$this->zip_code = $zip_code;
@@ -11,6 +12,7 @@ class GoogleWeather {
 		
 		$this->location = (string)$this->google_xml->weather->forecast_information->city->attributes();
 		$this->current_temp = (string)$this->google_xml->weather->current_conditions->temp_f->attributes();
+		$this->current_wind = (string)$this->google_xml->weather->current_conditions->wind_condition->attributes();
 	}
 	
 	private function create_query_url(){
@@ -30,6 +32,11 @@ class GoogleWeather {
 	# returns current temperature
 	function getcurrenttemp(){
 		return $this->current_temp;
+	}
+	
+	# returns current wind conditions
+	function getcurrentwind(){
+		return $this->current_wind;
 	}
 }
 ?>
